3aadc48b71e9501afb217f99670dbe359451d2c4
shobola faruq <shobolafaruq@gmail.com>
2024-08-29 00:46:10 +0000

    Add Register component UI

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import AuthLayout from "./components/layout/AuthLayout";
 import DashboardLayout from "./components/layout/DashboardLayout";
+import Register from "./components/Register";
 
 function App() {
   return (
     <Routes>
       <Route element={<AuthLayout />}>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Route>
 
       <Route element={<DashboardLayout />}>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import smallTeamDiscussionIdeas from "../assets/small-team-discussing-ideas.png";
 
-const Login = () => {
+const Reg = () => {
   return (
     <section className="relative flex flex-wrap items-center max-auto">
       <div className="w-full p-8 sm:px-8 lg:w-1/2 flex flex-col gap-8 border rounded-lg border-gray-200">
         <div className="">
           <p className="text-lg">Welcome!</p>
 
-          <h1 className="mt-6 text-2xl font-bold sm:text-3xl">Sign in to</h1>
+          <h1 className="mt-6 text-2xl font-bold sm:text-3xl">Sign up to</h1>
           <p className="mt-2 text-gray-500">Lorem ipsum is simply</p>
         </div>
         <form action="#" className="space-y-6">
@@ -21,7 +21,21 @@ const Login = () => {
               <input
                 type="email"
                 className="w-full rounded- border-2 border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                placeholder="Enter email"
+                placeholder="Enter your email"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label for="email" className="capitalize">
+              username
+            </label>
+
+            <div className="relative">
+              <input
+                type="text"
+                className="w-full rounded- border-2 border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                placeholder="Enter your username"
               />
             </div>
           </div>
@@ -35,7 +49,7 @@ const Login = () => {
               <input
                 type="password"
                 className="w-full rounded- border-2 border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                placeholder="Enter password"
+                placeholder="Enter your password"
               />
 
               <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
@@ -63,37 +77,54 @@ const Login = () => {
             </div>
           </div>
 
-          <div className="flex justify-between">
-            <div class="col-span-6">
-              <label for="MarketingAccept" class="flex gap-4">
-                <input
-                  type="checkbox"
-                  id="MarketingAccept"
-                  name="marketing_accept"
-                  class="size-5 rounded-md border-gray-200 bg-white shadow-sm"
-                />
+          <div>
+            <label for="password" className="capitalize">
+              confirm Password
+            </label>
 
-                <span class="text-sm text-gray-700">Remember me</span>
-              </label>
+            <div className="relative">
+              <input
+                type="password"
+                className="w-full rounded- border-2 border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                placeholder="Confirm your password"
+              />
+
+              <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                  />
+                </svg>
+              </span>
             </div>
-            <p className="text-center text-sm text-gray-500 capitalize">
-              <Link className="underline" to="/forgot-password">
-                Forget password?
-              </Link>
-            </p>
           </div>
 
           <button
             type="submit"
             className="block w-full rounded-md bg-black px-5 py-3 text-sm font-medium text-white"
           >
-            Sign in
+            Register
           </button>
 
           <p className="text-center text-sm text-gray-900">
-            Don't have an account?
-            <Link className="font-semibold ml-2" to="/register">
-              Register
+            Already have an account?
+            <Link className="font-semibold ml-2" to="/login">
+              Login
             </Link>
           </p>
         </form>
@@ -110,4 +141,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Reg;
